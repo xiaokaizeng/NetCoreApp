8c07f97965bd99f8189579f0544bbd453f0aa416
xiaobai <1007260412@qq.com>
2018-07-24 15:56:33 +0800

    Initial commit

---
+++ NetCoreApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////“Hello World”应用程序
+            //Console.WriteLine("Hello World!");
+            //Console.Write("Press any key to continue...");
+            //Console.ReadKey(true);
+
+            //改进“Hello World”应用程序
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+        }
+    }
+}
